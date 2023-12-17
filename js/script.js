9fbe334067182858703f5733378336e45a20a71c
DetailingCode <doniek77@o2.pl>
2023-12-17 22:26:39 +0100

    second test

---
+++ js/script.js
@@ -0,0 +1,84 @@
+const x = 50;
+
+if (x >= 100) {
+    console.log(`X > 100`)
+} 
+
+
+Identyfikator spotkania: 826 3203 0404
+Kod dostępu: 978651
+// const x = 10;
+
+
+// const check = (x % 2 == 0) ? 'x jest parzyste' : 'x jest nie parzyste'
+// console.log(check);
+
+
+
+
+// const x = 100;
+// const y = 50;
+
+// if (x > y) {
+// 	console.log(`${x}>${y}`);
+// } else if ((x === y)) {
+// 	console.log(`${x}=${y}`);
+// } else {
+// 	console.log(`${x}<${y}`);
+// }
+
+// const color = 'blue'
+// const newColor = 'green'
+
+// if(color === newColor) {
+// 	console.log('kolor się zgadzają')
+// } else{
+// 	console.log('kolory się nie zgadzają')
+// }
+
+// const password = "1tg5gh5fafafa";
+
+// if (password.length > 10 && password.includes('!')) {
+// 	console.log("Masz bardzo dobre hasło");
+// } else if (password.length > 10) {
+// 	console.log("Masz dobre hasło");
+// } else {
+// 	console.log("Masz na krótkie hasło");
+// }
+
+// switch (day) {
+// 	case "poniedzialek":
+// 		console.log("Dziś jest poniedziałek");
+// 		break;
+
+// 	case "wtorek":
+// 		console.log("Dziś jest wtorek");
+// 		break;
+
+// 	case "środa":
+// 		console.log("Dziś jest środa");
+// 		break;
+
+// 	case "czwartek":
+// 		console.log("Dziś jest czwartek");
+// 		break;
+
+// 	case "piątek":
+// 		console.log("Dziś jest piątek");
+// 		break;
+
+// 	default:
+// 		console.log("Weekend!");
+// }
+
+// const x = 100;
+
+// if (x > 20) {
+// 	console.log(`${x} > 20`);
+// } else {
+// 	console.log(`${x} < 20`);
+// }
+
+// const newX = x > 20 ? `${x} > 20` : `${x} < 20`;
+
+// console.log(newX);
